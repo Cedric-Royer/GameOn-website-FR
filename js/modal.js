757fed18f7c69aecce09f87d482a3bf5bf4daf4b
Cedric Royer <cedricroyerpro@gmail.com>
2024-07-09 18:26:06 +0200

    add required values to modal validation form

--- js/modal.js
+++ js/modal.js
@@ -12,7 +12,6 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCloseBtn = document.querySelector(".close");
-const modalValidBtn = document.querySelector(".btn-submit");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -30,92 +29,166 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-
-
-// check required input in modal form
+// check if all data format is correct before validate
 function validate() {
-  // Get form values
-  const firstName = document.getElementById('first');
-  const lastName = document.getElementById('last');
-  const email = document.getElementById('email');
-  const birthdate = document.getElementById('birthdate');
-  const quantity = document.getElementById('quantity');
-  const location = document.querySelector('input[name="location"]:checked');
-  const checkbox1 = document.getElementById('checkbox1').checked;
 
-  // Remove previous error messages and classes
-  document.querySelectorAll('.formData').forEach(el => {
-      el.removeAttribute('data-error');
-      el.removeAttribute('data-error-visible');
-  });
+  const formElements = {
+    firstName: { element: document.getElementById('first'), required: true },
+    lastName: { element: document.getElementById('last'), required: true },
+    email: { element: document.getElementById('email'), required: true },
+    birthdate: { element: document.getElementById('birthdate'), required: true },
+    quantity: { element: document.getElementById('quantity'), required: true},
+    location: { element: document.querySelector('input[name="location"]'),
+                elementDetails: document.querySelector('input[name="location"]:checked'),
+                required: true },
+    useConditions: { element: document.getElementById('checkbox1'), required: true }
+  };
 
-  // Validation flags
   let isValid = true;
 
-  // Helper function to add error message
-  function addErrorMessage(element, message) {
-      const parent = element.closest('.formData');
-      parent.setAttribute('data-error', message);
-      parent.setAttribute('data-error-visible', 'true');
-      element.classList.add('invalid');
+  clearData();
+
+  if (formElements.firstName.required === true) {
+    checkFirstName();
+  }
+  if (formElements.lastName.required === true){
+    checkLastName();
+  }
+  if (formElements.email.required === true){
+    checkEmail();
+  }
+  if (formElements.birthdate.required === true){
+    checkBirthday();
+  }
+  if (formElements.quantity.required === true){
+    checkTournoiQuantity();
+  }
+  if (formElements.location.required === true){
+    checkLocation();
+  }
+  if (formElements.useConditions.required === true){
+    checkUseConditions();
+  }
+  
+  if (isValid) {
+    submitForm();
+  }
+  
+  function submitForm() {
+      const reservationForm = document.getElementById("form-reservation");
+      const submitForm = document.getElementById("form-submit");
+      submitForm.setAttribute("type", "submit");
+      reservationForm.submit();
+  }
+
+  // Remove previous error messages and classes
+  function clearData() {
+    formData.forEach(el => {
+      el.removeAttribute('data-error');
+      el.removeAttribute('data-error-visible');
+      el.removeAttribute('data-valid');
+    });
   }
 
   // Validate first name
-  if (firstName.value.length < 2) {
+  function checkFirstName() {
+    const firstName = formElements.firstName.element;
+    if (firstName.value.length < 2) {
       addErrorMessage(firstName, "Le prénom doit comporter au moins 2 caractères.");
       isValid = false;
   } else {
-      firstName.classList.add('valid');
-  }
-
-  // Validate last name
-  if (lastName.value.length < 2) {
-      addErrorMessage(lastName, "Le nom doit comporter au moins 2 caractères.");
-      isValid = false;
-  } else {
-      lastName.classList.add('valid');
-  }
-
-  // Validate email
-  if (email.value.trim() === '') {
-    addErrorMessage(email, "L'adresse email est requise.");
-    isValid = false;
-  } else {
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    if (!emailPattern.test(email.value)) {
-      addErrorMessage(email, "L'adresse email n'est pas valide.");
-      isValid = false;
-    } else {
-      setValid(email);
+      addValidInput(firstName);
     }
   }
 
-  // Validate quantity
-  if (isNaN(quantity.value) || quantity.value < 0 || quantity.value > 99) {
-      addErrorMessage(quantity, "La quantité doit être un nombre entre 0 et 99.");
+  // Validate last name
+  function checkLastName() {
+    const lastName = formElements.lastName.element;
+    if (lastName.value.length < 2) {
+      addErrorMessage(lastName, "Le nom doit comporter au moins 2 caractères.");
       isValid = false;
   } else {
-      quantity.classList.add('valid');
+      addValidInput(lastName);
+    }
   }
+
+  // Validate email
+  function checkEmail() {
+    const email = formElements.email.element;
+    if (email.value.trim() === '') {
+      addErrorMessage(email, "Veuillez saisir votre adresse email.");
+      isValid = false;
+    } else {
+      const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+      if (!emailPattern.test(email.value)) {
+        addErrorMessage(email, "L'adresse email n'est pas valide.");
+        isValid = false;
+      } else {
+        addValidInput(email);
+      }
+    }
+  }
+
+  //Validate birthday
+  function checkBirthday() {
+    const birthdate = formElements.birthdate.element;
+    if (birthdate.value.trim() === '') {
+      addErrorMessage(birthdate, "Veuillez remplir votre date de naissance.");
+      isValid = false;
+    } else {
+      addValidInput(birthdate);
+    }
+  }
+
+  // Validate numbers tournois
+  function checkTournoiQuantity() {
+    const quantity = formElements.quantity.element;
+    if (quantity.value.trim() === '') {
+      addErrorMessage(quantity, "Veuillez saisir le nombre de tournois auxquels vous avez participé.");
+      isValid = false;
+    }
+    else if (isNaN(quantity.value)) {
+      addErrorMessage(quantity, "Le nombre ne doit pas être écris en lettres.");
+      isValid = false;
+    } 
+    else if (quantity.value < 0 || quantity.value > 99) {
+      addErrorMessage(quantity, "Le nombre doit être compris entre 0 et 99.");
+      isValid = false;
+    } else {
+      addValidInput(quantity);
+    }
+  }  
 
   // Validate location
-  if (!location) {
-      addErrorMessage(document.querySelector('input[name="location"]'), "Veuillez sélectionner un lieu.");
+  function checkLocation() {
+    const location = formElements.location.element;
+    const locationDetails = formElements.location.elementDetails;
+    if (!locationDetails) {
+      addErrorMessage(location, "Veuillez sélectionner un lieu.");
       isValid = false;
-  } else {
-      document.querySelectorAll('input[name="location"]').forEach(el => el.classList.add('valid'));
+    } 
   }
 
-  // Validate checkbox1
-  if (!checkbox1) {
-      addErrorMessage(document.getElementById('checkbox1'), "Vous devez accepter les conditions d'utilisation.");
+  // Validate use conditions checked
+  function checkUseConditions() {
+    const useConditions = formElements.useConditions.element;
+    if (!useConditions.checked) {
+      addErrorMessage(useConditions, "Vous devez accepter les conditions d'utilisation.");
       isValid = false;
-  } else {
-      document.getElementById('checkbox1').classList.add('valid');
+    } 
   }
 
-  // Prevent form submission if validation fails
-  return isValid;
+  // Add error message
+  function addErrorMessage(element, message) {
+    const parent = element.closest('.formData');
+    parent.setAttribute('data-error', message);
+    parent.setAttribute('data-error-visible', 'true');
+  }
+
+  // Change color if it's valid
+  function addValidInput(element) {
+    const parent = element.closest('.formData');
+    parent.setAttribute('data-valid', 'true');
+  }
 }
-
 
